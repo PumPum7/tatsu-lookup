bb45dfb5e893184310675299c764b31e7c080f91
Pum <28806828+PumPum7@users.noreply.github.com>
2021-10-17 11:48:12 +0200

    added a GitHub link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 
+import GithubIcon from "@components/icons/GithubIcon";
 import SearchIcon from "@components/icons/SearchIcon";
+
+const GITHUB_URL = "https://github.com/PumPum7/tatsu-lookup";
 
 const Header = (): JSX.Element => {
     return (
-        <header className="flex justify-center items-center bg-tatsuGreen text-white">
+        <header className="flex justify-center items-center bg-tatsuGreen text-white relative">
             <SearchIcon className="sm:w-14 sm:h-14 w-8 h-8" />
-            <h1 className="px-5 text-4xl font-bold leading-tight tracking-tight text-center sm:mt-4 sm:text-6xl md:pb-4">
+            <h1 className="px-3 text-4xl font-bold leading-tight tracking-tight text-center sm:mt-4 sm:text-6xl sm:pb-4 pb-2">
                 Tatsu Lookup
             </h1>
+            <a
+                href={GITHUB_URL}
+                rel="noopener noreferrer"
+                target="_blank"
+                className="sm:absolute sm:right-4"
+                title="Github"
+            >
+                <GithubIcon className="w-8 h-8 sm:w-12 sm:h-12" />
+            </a>
         </header>
     );
 };
